21e6bcf39e157caca9a82f355a51201ffac55b17
hierdogan <ibrahimerdogan@gmail.com>
2026-01-27 14:47:27 +0300

    ilk kurulum adımları

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import plotly.express as px
 
 # We create a fixed dataframe
 
+# BU DEĞİŞİKLİK YENİ BRUNCH first-step üzerinde yapıldı
+
 data = {
 'Time': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
 'Cost': [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
